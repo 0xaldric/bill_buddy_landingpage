a306913f46c15461c6fcbe7d4360c2e8d8aa5082
0xaldric <anhnd350309@gmail.com>
2025-11-06 18:15:27 +0700

    chore: update

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Hero } from './Hero'
-export { default as HowItWorks } from './HowItWorks'
-export { default as Features } from './Features'
-export { default as Testimonials } from './Testimonials'
-export { default as CTA } from './CTA'
-export { default as Footer } from './Footer'
-export { default as QueryProvider } from './QueryProvider'
-
-
